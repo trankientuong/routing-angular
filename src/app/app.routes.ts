72d41af3f0e389000e236287c86b866d2d149ddd
tuongtran1106 <tuongtran1106@gmail.com>
2024-07-26 10:56:47 +0700

    Registering multiple routes & Extracting Dynamic Route Parameters via Inputs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from "@angular/router";
-import { TasksComponent } from "./tasks/tasks.component";
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
+import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 
 export const routes: Routes = [
   {
-    path: 'tasks', // <your-domain>/tasks
-    component: TasksComponent,
+    path: '', // <your-domain>/
+    component: NoTaskComponent
+  },
+  {
+    path: 'users/:userId', // <your-domain>/users/<uid>
+    component: UserTasksComponent
   },
 ];
