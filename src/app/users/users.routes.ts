6bfbc2d412c4454eaf1f4ef77332a2b167ad90d3
tuongtran1106 <tuongtran1106@gmail.com>
2024-07-29 14:55:20 +0700

    Apply canDeactive for new task component

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from "@angular/router";
 import { resolveUserTasks, TasksComponent } from "../tasks/tasks.component";
-import { NewTaskComponent } from "../tasks/new-task/new-task.component";
+import { canLeaveEditPage, NewTaskComponent } from "../tasks/new-task/new-task.component";
 
 export const routes: Routes = [
     {
@@ -18,6 +18,7 @@ export const routes: Routes = [
       },
       {
         path: 'tasks/new',
-        component: NewTaskComponent
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage]
       }
 ]
